5ef6354fbd5f9afc406f33b1e4d300eee70d127b
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-10-18 22:30:09 +0530

    seatlayout

--- src/services/checkout/component/PriceSummary.jsx
+++ src/services/checkout/component/PriceSummary.jsx
@@ -10,7 +10,7 @@ const PriceSummary = () => {
           </h1>
           <div>
             <div className=" px-2 py-2 space-y-2">
-              <ul className="flex items-center justify-between  text-primary font-semibold border-b-2 border-dashed border-gray-400">
+              <ul className="flex items-center justify-between  text-primary  font-semibold border-b-2 border-dashed border-gray-400">
                 <li>Item Desc</li>
                 <li>Base Fare</li>
                 <li>GST</li>
@@ -18,7 +18,7 @@ const PriceSummary = () => {
               </ul>
             </div>
             <div className=" px-2 py-2">
-              <ul className="flex items-center justify-between space-y-2 border-b-2 border-dashed border-gray-400">
+              <ul className="flex items-center justify-between text-sm lg:text-md space-y-2 border-b-2 border-dashed border-gray-400">
                 <li>Trip Fare (6 berth)</li>
                 <li>₹7494</li>
                 <li>₹378</li>
